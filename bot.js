1105c0c98bfc0da01e3962f9c2852dab0b6891b6
Ahmed-Rizeq <45902728+Ahmed-Rizeq@users.noreply.github.com>
2018-12-15 10:49:27 -0800

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("523568446034935840")
+setInterval(function() {
+channel.send(`منورين السيرفر`);
+}, 30)
+})
+
+client.login(NTEwODMyNTY2OTUwMDM1NDU2.DtbvfA.9qQSxD6bRKxOiz0ClTZkCBTSxzA);
